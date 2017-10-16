96d0c840b82c472090abb88d5f303c1db7648919
Hamza Sentissi <hamza.sentissi@hec.edu>
2017-10-16 14:52:34 +0200

    blala

--- R/hamza.R
+++
@@ -1,9 +0,0 @@
-#' Toolkit functions to start with R - Hamza
-#'
-#' This package is a toolkit to help get started with R
-#'
-#' @name hamza-package
-#' @aliases hamza-package hamza
-#' @docType package
-#' @author Hamza <hamza.sentissi@polytechnique.edu>
-NULL
